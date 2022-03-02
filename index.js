54ebb0a6712b0af24c4ee455ee289a75f7f5d5ef
chi-wei-lien <lien2@purdue.edu>
2022-03-01 20:38:47 -0500

    update bg

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var today = new Date();
+var canvas = document.getElementById("dark-mode-canvas");
 var bg = document.getElementById("dark-mode-bg");
 var words = document.getElementsByClassName("dark-mode-word");
 var nav = document.getElementById("dark-mode-nav");
 
 if (today.getHours() > 17) {
-  bg.style.backgroundColor = "#4C6079";
+  canvas.style.backgroundColor = "#4C6079";
+  bg.style.backgroundColor = "#343a40";
   for (let word of words) {
     word.style.color = "white";
   }
